08900606afce286145e67cb9f3b6d6a1705d25b8
aks13raut <aks13raut@gmail.com>
2019-12-26 23:59:20 +0530

    MessageBus seprate class

--- src/Messaging/System.hpp
+++ src/Messaging/System.hpp
@@ -1,22 +1,6 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <functional>
-
-#include "Message.hpp"
-
-class MessageBus {
-    std::vector<std::function<void (Message)>> receivers;
-    std::queue<Message> messages;
-    public:
-    MessageBus();
-    ~MessageBus();
-    void addReceiver(std::function<void (Message)> messageReceiver);
-    void sendMessage(Message _message);
-    void notify();
-};
+#include "MessageBus.hpp"
 
 class System {
     protected:
